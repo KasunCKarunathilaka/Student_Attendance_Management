5015bac1b6e8a728258ac76e539d16ece666bbad
KasunCKarunathilaka <kasunckarunathilaka@gmail.com>
2026-08-11 20:14:44 +0530

    building admin-view UI

---
+++ sams/src/main/java/lk/ijse/sams/controller/AdminViewController.java
@@ -0,0 +1,71 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Button attendanceBtn;
+
+    @FXML
+    private Button classSheduleBtn;
+
+    @FXML
+    private Button courseMgtBtn;
+
+    @FXML
+    private Button lectureMgtBtn;
+
+    @FXML
+    private Button logOutBtn;
+
+    @FXML
+    private Button reportBtn;
+
+    @FXML
+    private Button studentMgtBtn;
+
+    @FXML
+    private Button userMgtBtn;
+
+    @FXML
+    void onClickAttendanceBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickClassSheduleBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickLectureMgtBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickLogOutBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickReportBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickStudentMgtBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickUserMgtBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickcourseMgtBtn(ActionEvent event) {
+
+    }
+
+}
